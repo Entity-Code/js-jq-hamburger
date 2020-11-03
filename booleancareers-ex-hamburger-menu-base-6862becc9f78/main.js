c085333b8762606d798ef3b0b92e869ee4a076d1
Mattia <mattia.tummolilllo@gmail.com>
2020-11-03 09:48:59 +0100

    versione con classe active

--- booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,31 +1,37 @@
-var icon = $(".header-right > a");
-var close = $(".close");
-
-var menu = $(".hamburger-menu");
-
-//apri menu
-icon.click(showMenu);
-
-//chiudi menu
-close.click(hideMenu);
-
-
+// var icon = $(".header-right");
+// var close = $(".close");
+//
+// var menu = $(".hamburger-menu");
+//
+// //apri menu
+// icon.click(showMenu);
+//
+// //chiudi menu
+// close.click(hideMenu);
 
 
-
-
-
-
-
-
+$('i.fas.fa-bars').click(function(){
+   $('.hamburger-menu').addClass("active");
+});
+$('i.fas.fa-times').click(function(){
+   $('.hamburger-menu').removeClass("active");
+});
 
 
 
 
-// functions
-function showMenu() {
-   menu.fadeIn(500);
-}
-function hideMenu(){
-   menu.fadeOut(500);
-}
+
+
+
+
+
+
+
+
+// // functions
+// function showMenu() {
+//    menu.fadeIn(500);
+// }
+// function hideMenu(){
+//    menu.fadeOut(500);
+// }
